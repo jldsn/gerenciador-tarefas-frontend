40c44200852e72f417369e56f62b79af196b5955
José Luiz <104479818+jldsn@users.noreply.github.com>
2025-03-05 19:57:25 -0300

    Update api.js
    Atualiza URL da API para o back-end em produção

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/tarefas',
+  baseURL: 'https://gerenciador-tarefas-jv2w.onrender.com/tarefas',
 });
 
 export const getTasks = async () => {
